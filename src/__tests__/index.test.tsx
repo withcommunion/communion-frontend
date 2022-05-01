776b2895c8a7d911847ac96bfc15295433a40c72
Mike Alicea <mfalicea58@gmail.com>
2022-05-01 13:35:04 -0400

    Strip away all index fat

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Index from '../pages/index';
 
-describe('Home', () => {
+describe('Index', () => {
   it('renders a heading', () => {
     render(<Index />);
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: /welcome to communion/i,
     });
 
     expect(heading).toBeInTheDocument();
