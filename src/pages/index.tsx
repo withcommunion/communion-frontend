daa78a5f347d8cd0a571d511b2f8dfc428ea8a09
Mike Alicea <mfalicea58@gmail.com>
2022-05-19 21:15:51 -0500

    Auth cleanup!  Not perfect,but is looking good

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import router from 'next/router';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
+import { AMPLIFY_CONFIG } from '../util/cognitoAuthUtil';
+
+// https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
+Amplify.configure({ ...AMPLIFY_CONFIG, ssr: false });
+
 const Index: NextPage = () => {
+  const { route } = useAuthenticator((context) => {
+    console.log(context.route);
+    if (context.user?.attributes?.email_verified === 'true') {
+      router.push('/demo');
+    }
+    return [context.route];
+  });
+
   useAuthenticator((context) => {
     console.log(context.user);
     const { user } = context;
@@ -13,6 +27,7 @@ const Index: NextPage = () => {
     }
     return [context.user];
   });
+
   const formFields = {
     signUp: {
       email: {
@@ -45,13 +60,19 @@ const Index: NextPage = () => {
         <h1 className="text-2xl">👋 Hey friend</h1>
         <h1 className="text-3xl">Welcome to Communion</h1>
         <h1 className="text-2xl">Good stuff coming soon!</h1>
-        <Authenticator
-          className="mt-5"
-          initialState="signIn"
-          signUpAttributes={['email', 'given_name', 'family_name']}
-          formFields={formFields}
-          loginMechanisms={['email']}
-        />
+        <div className="">
+          {route === 'idle' || !route ? (
+            <h1 className="my-32">loading</h1>
+          ) : (
+            <Authenticator
+              className="mt-5"
+              initialState="signIn"
+              signUpAttributes={['email', 'given_name', 'family_name']}
+              formFields={formFields}
+              loginMechanisms={['email']}
+            />
+          )}
+        </div>
       </main>
     </div>
   );
--- src/util/cognitoAuthUtil.ts
+++ src/util/cognitoAuthUtil.ts
@@ -1,5 +1,43 @@
+import { withSSRContext } from 'aws-amplify';
+import type { Auth } from 'aws-amplify';
+import type { CognitoUser } from 'amazon-cognito-identity-js';
+import { GetServerSidePropsContext } from 'next';
+
 export const USER_POOL_ID_DEV = 'us-east-1_EXRZZF0cp';
 export const USER_POOL_ID_PROD = 'us-east-1_SeeaUyuuH';
 
 export const USER_POOL_CLIENT_ID_DEV = '4eerlu1taf72c8r20pv2tmmvmt';
 export const USER_POOL_CLIENT_ID_PROD = '66eoq77778g7d8e36v6pobj0b6';
+
+export const AMPLIFY_CONFIG = {
+  aws_cognito_region: 'us-east-1', // (required) - Region where Amazon Cognito project was created
+  aws_user_pools_id: USER_POOL_ID_DEV, // (optional) -  Amazon Cognito User Pool ID
+  aws_user_pools_web_client_id: USER_POOL_CLIENT_ID_DEV, // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
+  // aws_cognito_identity_pool_id:
+  //   'us-east-1:f602c14b-0fde-409c-9a7e-0baccbfd87d0', // (optional) - Amazon Cognito Identity Pool ID
+  aws_mandatory_sign_in: 'enable', // (optional) - Users are not allowed to get the aws credentials unless they are signed in
+  cookieStorage: {
+    domain: 'localhost',
+    // Set true if is a domain with https. For localhost set it to false
+    secure: false,
+    path: '/',
+    expires: 2,
+  },
+};
+
+export async function getUserJwtTokenOnServer(
+  serverSidePropsContext: GetServerSidePropsContext
+) {
+  const SSRContext = withSSRContext(serverSidePropsContext);
+  const SsrAuth = SSRContext.Auth as typeof Auth;
+
+  let userJwt;
+  try {
+    const user = (await SsrAuth.currentAuthenticatedUser()) as CognitoUser;
+    userJwt = user.getSignInUserSession()?.getIdToken().getJwtToken();
+    return userJwt;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
