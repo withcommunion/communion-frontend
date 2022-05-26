6be60eda0ddbd5e81155082e513a65aaeabcf196
Mike Alicea <mfalicea58@gmail.com>
2022-05-26 11:45:25 -0500

    Lists users + updates look and feel

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -4,7 +4,12 @@ import { ethers } from 'ethers';
 import { Amplify } from 'aws-amplify';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
-import { fetchSelf, Self } from '@/util/walletApiUtil';
+import {
+  fetchSelf,
+  Self,
+  fetchOrganization,
+  Organization,
+} from '@/util/walletApiUtil';
 import { getEthersWallet } from '@/util/avaxEthersUtil';
 import { AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
 import Router from 'next/router';
@@ -14,78 +19,114 @@ import { getUserJwtTokenOnServer } from '@/util/cognitoAuthUtil';
 // https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
 
-const BasicWalletDemo = ({ userJwt }: { userJwt: string }) => {
+interface Props {
+  userJwt: string;
+  self: Self;
+}
+
+const BasicWalletDemo = ({ userJwt, self }: Props) => {
   const [userPrivateKey, setUserPrivateKey] = useState<string>('');
   const [ethersWallet, setEthersWallet] = useState<ethers.Wallet>();
   const [accountBalance, setAccountBalance] = useState<string>();
-  const [self, setSelf] = useState<Self>();
+  const [organization, setOrganization] = useState<Organization>();
+  // const [self, setSelf] = useState<Self>();
 
   const { signOut } = useAuthenticator((context) => [context.signOut]);
 
   useEffect(() => {
-    if (userPrivateKey) {
-      console.log('here');
-      setEthersWallet(getEthersWallet(userPrivateKey));
+    if (!userPrivateKey && self && self.wallet.privateKeyWithLeadingHex) {
+      setUserPrivateKey(self.wallet.privateKeyWithLeadingHex);
+      setEthersWallet(getEthersWallet(self.wallet.privateKeyWithLeadingHex));
     }
-  }, [userPrivateKey]);
+  }, [self, userPrivateKey]);
 
   useEffect(() => {
     const fetchBalance = async (wallet: ethers.Wallet) => {
       const balanceBigNumber = await wallet.getBalance();
-      console.log(balanceBigNumber);
-      console.log(ethers.utils.formatEther(balanceBigNumber));
       setAccountBalance(ethers.utils.formatEther(balanceBigNumber));
     };
 
-    if (ethersWallet) {
+    if (ethersWallet && !accountBalance) {
       fetchBalance(ethersWallet);
     }
-  }, [ethersWallet]);
+  }, [ethersWallet, accountBalance]);
+
+  useEffect(() => {
+    const fetchOrg = async () => {
+      const org = await fetchOrganization(self.organization, userJwt);
+      console.log(org);
+      setOrganization(org);
+    };
+
+    if (!organization && self && userJwt) {
+      fetchOrg();
+    }
+  }, [self, organization, userJwt]);
 
   return (
-    <div className="h-screen container flex flex-col justify-center items-center">
-      <h1>here</h1>
-      {userJwt && (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={async () => {
-            const self = await fetchSelf(userJwt);
-            const userPrivateKey = self.wallet.privateKeyWithLeadingHex;
+    <div className="h-screen w-screen p-5">
+      {self && (
+        <div className="container flex flex-col items-center">
+          <h2>👋 Welcome {self.first_name}!</h2>
+          {ethersWallet && (
+            <>
+              <p>Your address:</p>
+              <div className="whitespace-normal break-words">
+                <p>
+                  <small>{ethersWallet.address}</small>
+                </p>
+              </div>
+            </>
+          )}
 
-            setSelf(self);
-            setUserPrivateKey(userPrivateKey);
-          }}
-        >
-          Connect to wallet
-        </button>
-      )}
-      {self && <h3>Hi {self.first_name}</h3>}
-      {ethersWallet && (
-        <div className="">
-          <p>Your address:</p>
-          <p>{ethersWallet.address}</p>
+          {accountBalance && (
+            <>
+              <p>Your balance:</p>
+              <p>{accountBalance} AVAX</p>
+            </>
+          )}
         </div>
       )}
-      {accountBalance && (
-        <div className="">
-          <p>Your balance:</p>
-          <p>{accountBalance} AVAX</p>
-        </div>
+
+      {organization && (
+        <ul className="mt-5 flex flex-col gap-y-4">
+          {organization.users.map((user) => (
+            <li
+              className="flex items-center gap-x-1"
+              key={`${user.wallet.addressC}`}
+            >
+              <p>
+                {user.first_name} {user.last_name}
+              </p>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+                onClick={() => {
+                  console.log(`Sending txn to ${user.wallet.addressC}`);
+                }}
+              >
+                Send
+              </button>
+            </li>
+          ))}
+        </ul>
       )}
+
       {userJwt && (
-        <div className="mt-5">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => {
-              signOut();
-              // TODO: This is hacky af
-              setTimeout(() => {
-                Router.push('/');
-              }, 500);
-            }}
-          >
-            Sign out
-          </button>
+        <div className="flex justify-center">
+          <div className="absolute bottom-5 mt-5">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                signOut();
+                // TODO: This is hacky af
+                setTimeout(() => {
+                  Router.push('/');
+                }, 500);
+              }}
+            >
+              Sign out
+            </button>
+          </div>
         </div>
       )}
     </div>
@@ -95,6 +136,13 @@ const BasicWalletDemo = ({ userJwt }: { userJwt: string }) => {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const userJwt = await getUserJwtTokenOnServer(context);
+    let self;
+    if (userJwt) {
+      self = await fetchSelf(userJwt);
+      return {
+        props: { userJwt, self },
+      };
+    }
     return {
       props: { userJwt },
     };
--- src/util/walletApiUtil.ts
+++ src/util/walletApiUtil.ts
@@ -4,7 +4,7 @@ export const DEV_API_URL =
   'https://p0rddetfk8.execute-api.us-east-1.amazonaws.com';
 
 export interface BaseUserWallet {
-  privateKeyWithLeadingHex: string;
+  privateKeyWithLeadingHex?: string;
   addressC: string;
   addressP: string;
   addressX: string;
@@ -13,10 +13,11 @@ export interface BaseUserWallet {
 export interface User {
   urn: string;
   id: string;
-  email: string;
+  email?: string;
   first_name: string;
   last_name: string;
   organization: string;
+  wallet: BaseUserWallet;
 }
 
 export interface Self extends User {
@@ -32,6 +33,31 @@ export async function fetchSelf(jwtToken: string): Promise<Self> {
     });
     const self = rawWallet.data;
     return self;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export interface Organization {
+  name: string;
+  users: User[];
+}
+export async function fetchOrganization(
+  orgName: string,
+  jwtToken: string
+): Promise<Organization> {
+  try {
+    const rawOrg = await axios.get<Organization>(
+      `${DEV_API_URL}/organization/${orgName}`,
+      {
+        headers: {
+          Authorization: jwtToken,
+        },
+      }
+    );
+    const organization = rawOrg.data;
+    return organization;
   } catch (error) {
     console.log(error);
     throw error;
