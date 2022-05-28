008a06a251ea3d3cc1983e68b69a0281f8c4de29
Mike Alicea <mfalicea58@gmail.com>
2022-05-28 15:16:32 -0500

    Adds role to User interface

--- src/util/walletApiUtil.ts
+++ src/util/walletApiUtil.ts
@@ -16,6 +16,7 @@ export interface User {
   first_name: string;
   last_name: string;
   organization: string;
+  role: 'worker' | 'manager' | 'owner' | 'seeder' | string;
   wallet: BaseUserWallet;
 }
 
