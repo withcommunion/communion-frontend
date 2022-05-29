bd05a43fc02a5d43c7245727199c1ac23e50f849
Mike Alicea <mfalicea58@gmail.com>
2022-05-29 14:28:20 -0500

    Adds stages for cookieStorage

--- src/util/cognitoAuthUtil.ts
+++ src/util/cognitoAuthUtil.ts
@@ -9,6 +9,25 @@ export const USER_POOL_ID_PROD = 'us-east-1_SeeaUyuuH';
 export const USER_POOL_CLIENT_ID_DEV = '4eerlu1taf72c8r20pv2tmmvmt';
 export const USER_POOL_CLIENT_ID_PROD = '66eoq77778g7d8e36v6pobj0b6';
 
+console.log(process.env.NEXT_PUBLIC_STAGE);
+const isDev = Boolean(process.env.NEXT_PUBLIC_STAGE == 'dev');
+
+const cookieStorageLocal = {
+  domain: 'localhost',
+  // Set true if is a domain with https. For localhost set it to false
+  secure: false,
+  path: '/',
+  expires: 30,
+};
+
+const cookieStorageVercel = {
+  domain: 'communion-frontend.vercel.app',
+  // Set true if is a domain with https. For localhost set it to false
+  secure: true,
+  path: '/',
+  expires: 30,
+};
+
 export const AMPLIFY_CONFIG = {
   aws_cognito_region: 'us-east-1', // (required) - Region where Amazon Cognito project was created
   aws_user_pools_id: USER_POOL_ID_DEV, // (optional) -  Amazon Cognito User Pool ID
@@ -16,13 +35,7 @@ export const AMPLIFY_CONFIG = {
   // aws_cognito_identity_pool_id:
   //   'us-east-1:f602c14b-0fde-409c-9a7e-0baccbfd87d0', // (optional) - Amazon Cognito Identity Pool ID
   aws_mandatory_sign_in: 'enable', // (optional) - Users are not allowed to get the aws credentials unless they are signed in
-  cookieStorage: {
-    domain: 'communion-frontend.vercel.app',
-    // Set true if is a domain with https. For localhost set it to false
-    secure: true,
-    path: '/',
-    expires: 2,
-  },
+  cookieStorage: isDev ? cookieStorageLocal : cookieStorageVercel, // (optional) - Cookie storage options
 };
 
 export async function getUserJwtTokenOnServer(
