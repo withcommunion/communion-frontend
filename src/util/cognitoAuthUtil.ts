a4f4b066f9cb88e69e369883e295b677e86ac801
Mike Alicea <mfalicea58@gmail.com>
2022-05-30 13:06:28 -0500

    make VERCEL_URL public to client

--- src/util/cognitoAuthUtil.ts
+++ src/util/cognitoAuthUtil.ts
@@ -12,17 +12,17 @@ export const USER_POOL_CLIENT_ID_PROD = '66eoq77778g7d8e36v6pobj0b6';
 console.log(process.env.NEXT_PUBLIC_STAGE);
 const isDev = Boolean(process.env.NEXT_PUBLIC_STAGE == 'dev');
 
-console.log(process.env.VERCEL_URL);
+console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
 
-const cookieStorageLocal = {
-  domain: process.env.VERCEL_URL || 'localhost',
+const cookieStorageDev = {
+  domain: process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost',
   // Set true if is a domain with https. For localhost set it to false
   secure: false,
   path: '/',
   expires: 30,
 };
 
-const cookieStorageProdDomain = {
+const cookieStorageProd = {
   domain: 'withcommunion.com',
   // Set true if is a domain with https. For localhost set it to false
   secure: true,
@@ -37,7 +37,7 @@ export const AMPLIFY_CONFIG = {
   // aws_cognito_identity_pool_id:
   //   'us-east-1:f602c14b-0fde-409c-9a7e-0baccbfd87d0', // (optional) - Amazon Cognito Identity Pool ID
   aws_mandatory_sign_in: 'enable', // (optional) - Users are not allowed to get the aws credentials unless they are signed in
-  cookieStorage: isDev ? cookieStorageLocal : cookieStorageProdDomain, // (optional) - Cookie storage options
+  cookieStorage: isDev ? cookieStorageDev : cookieStorageProd, // (optional) - Cookie storage options
 };
 
 export async function getUserJwtTokenOnServer(
