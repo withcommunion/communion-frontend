2275c9c02704909b8b025977c4820ec3afb95420
Mike Alicea <mfalicea58@gmail.com>
2022-05-30 12:13:12 -0500

    Remove *.vercel.app, it doesn't work

--- src/util/cognitoAuthUtil.ts
+++ src/util/cognitoAuthUtil.ts
@@ -21,7 +21,7 @@ const cookieStorageLocal = {
 };
 
 const cookieStorageVercel = {
-  domain: '*.vercel.app',
+  domain: 'communion-frontend.vercel.app',
   // Set true if is a domain with https. For localhost set it to false
   secure: true,
   path: '/',
