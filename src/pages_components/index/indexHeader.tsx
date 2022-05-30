c03c8b0206a7691368c1aee4fc445b1e5bb24ea5
Mike Alicea <mfalicea58@gmail.com>
2022-05-30 12:04:19 -0500

    Pass username to indexHeader

--- src/pages_components/index/indexHeader.tsx
+++ src/pages_components/index/indexHeader.tsx
@@ -1,7 +1,13 @@
-const IndexHeader = () => {
+interface Props {
+  userName?: string;
+}
+const IndexHeader = ({ userName }: Props) => {
   return (
     <>
-      <h1 className="text-2xl">👋 Hey friend</h1>
+      <h1 className="text-2xl">
+        {userName && `👋 Hey ${userName}`}
+        {!userName && '👋 Hey friend'}
+      </h1>
       <h1 className="text-3xl">Welcome to Communion</h1>
       <h1 className="text-2xl">Good stuff coming soon!</h1>
     </>
