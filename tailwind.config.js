dc4e74c7f92940848d4a74c6bcfde6088685484b
Mike Alicea <mfalicea58@gmail.com>
2022-06-03 15:02:39 -0500

    Adds and unifies more VH

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const vhMap = {
+  '10vh': '10vh',
+  '25vh': '25vh',
+  '35vh': '35vh',
+  '50vh': '50vh',
+  '65vh': '65vh',
+  '75vh': '75vh',
+  '85vh': '85vh',
+  '95vh': '95vh',
+};
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      height: {
-        '50vh': '50vh',
-        '65vh': '65vh',
-        '75vh': '75vh',
-      },
+      height: vhMap,
+      maxHeight: vhMap,
     },
   },
   plugins: [],
