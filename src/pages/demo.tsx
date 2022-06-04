ce2cb51442ba84e8ded6c5010fa8bb5df2adacf6
Mike Alicea <mfalicea58@gmail.com>
2022-06-04 12:54:20 -0500

    Adds simple simple navbar

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -12,9 +12,9 @@ import {
 } from '@/util/walletApiUtil';
 import { getEthersWallet, sendAvax } from '@/util/avaxEthersUtil';
 import { AMPLIFY_CONFIG } from '@/util/cognitoAuthUtil';
-import { useRouter } from 'next/router';
 
 import { getUserJwtTokenOnServer } from '@/util/cognitoAuthUtil';
+import NavBar from '@/shared_components/navBar';
 
 // https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
@@ -25,8 +25,6 @@ interface Props {
 }
 
 const BasicWalletDemo = ({ userJwt, self }: Props) => {
-  const router = useRouter();
-
   const [userPrivateKey, setUserPrivateKey] = useState<string>('');
   const [ethersWallet, setEthersWallet] = useState<ethers.Wallet>();
   const [accountBalance, setAccountBalance] = useState<string>();
@@ -115,147 +113,135 @@ const BasicWalletDemo = ({ userJwt, self }: Props) => {
   };
 
   return (
-    <div className="py-4 flex flex-col items-center ">
-      <div className="w-full md:w-1/4 px-5">
-        {self && (
-          <div className="container flex flex-col items-center">
-            <h2>👋 Welcome {self.first_name}!</h2>
-            {ethersWallet && (
-              <>
-                <p>Your address:</p>
-                <div className="whitespace-normal break-words">
-                  <p>
-                    <small>
-                      <a
-                        className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                        target="_blank"
-                        rel="noreferrer"
-                        href={`https://testnet.snowtrace.io/address/${ethersWallet.address}`}
-                      >
-                        {`${ethersWallet.address.substring(
-                          0,
-                          10
-                        )}...${ethersWallet.address.substring(32)}`}
-                      </a>
-                    </small>
-                  </p>
-                </div>
-              </>
-            )}
+    <>
+      <NavBar signOut={signOut} active="send" />
+      <div className="py-4 flex flex-col items-center ">
+        <div className="w-full md:w-1/4 px-5">
+          {self && (
+            <div className="container flex flex-col items-center">
+              <h2>👋 Welcome {self.first_name}!</h2>
+              {ethersWallet && (
+                <>
+                  <p>Your address:</p>
+                  <div className="whitespace-normal break-words">
+                    <p>
+                      <small>
+                        <a
+                          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                          target="_blank"
+                          rel="noreferrer"
+                          href={`https://testnet.snowtrace.io/address/${ethersWallet.address}`}
+                        >
+                          {`${ethersWallet.address.substring(
+                            0,
+                            10
+                          )}...${ethersWallet.address.substring(32)}`}
+                        </a>
+                      </small>
+                    </p>
+                  </div>
+                </>
+              )}
 
-            {accountBalance && (
-              <>
-                <p>Your balance:</p>
-                <p>
-                  {latestTransaction?.isInProgress && <small>♻️</small>}
-                  {accountBalance} AVAX
-                </p>
-                {isAccountBalanceZero && ethersWallet && (
+              {accountBalance && (
+                <>
+                  <p>Your balance:</p>
+                  <p>
+                    {latestTransaction?.isInProgress && <small>♻️</small>}
+                    {accountBalance} AVAX
+                  </p>
+                  {isAccountBalanceZero && ethersWallet && (
+                    <button
+                      className="bg-blue-500 disabled:bg-gray-400 hover:bg-blue-700 text-white py-1 px-2 rounded"
+                      disabled={isAccountBalanceZeroLoading}
+                      onClick={async () => {
+                        setIsAccountBalanceZeroLoading(true);
+                        const balance = await ethersWallet.getBalance();
+                        setIsAccountBalanceZero(balance.isZero());
+                        setAccountBalance(ethers.utils.formatEther(balance));
+                        setIsAccountBalanceZeroLoading(false);
+                      }}
+                    >
+                      Balance is zero? Refresh!
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+          )}
+
+          {organization && (
+            <ul className="mt-5 h-50vh flex flex-col items-start gap-y-3 overflow-auto">
+              {organization.users.map((user) => (
+                <li
+                  className="flex justify-between items-center w-full gap-x-2"
+                  key={`${user.wallet.addressC}`}
+                >
+                  <p>
+                    {user.first_name} {user.last_name}
+                  </p>
                   <button
                     className="bg-blue-500 disabled:bg-gray-400 hover:bg-blue-700 text-white py-1 px-2 rounded"
-                    disabled={isAccountBalanceZeroLoading}
+                    disabled={latestTransaction?.isInProgress}
                     onClick={async () => {
-                      setIsAccountBalanceZeroLoading(true);
-                      const balance = await ethersWallet.getBalance();
-                      setIsAccountBalanceZero(balance.isZero());
-                      setAccountBalance(ethers.utils.formatEther(balance));
-                      setIsAccountBalanceZeroLoading(false);
+                      if (ethersWallet) {
+                        await sendUserAvax(ethersWallet, user.wallet.addressC);
+                      }
                     }}
                   >
-                    Balance is zero? Refresh!
+                    Send
                   </button>
-                )}
-              </>
-            )}
-          </div>
-        )}
-
-        {organization && (
-          <ul className="mt-5 h-50vh flex flex-col items-start gap-y-3 overflow-auto">
-            {organization.users.map((user) => (
-              <li
-                className="flex justify-between items-center w-full gap-x-2"
-                key={`${user.wallet.addressC}`}
-              >
-                <p>
-                  {user.first_name} {user.last_name}
-                </p>
-                <button
-                  className="bg-blue-500 disabled:bg-gray-400 hover:bg-blue-700 text-white py-1 px-2 rounded"
-                  disabled={latestTransaction?.isInProgress}
-                  onClick={async () => {
-                    if (ethersWallet) {
-                      await sendUserAvax(ethersWallet, user.wallet.addressC);
-                    }
-                  }}
-                >
-                  Send
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-        {latestTransaction && (
-          <div className="mt-10">
-            <ul>
-              {latestTransaction.isInProgress && (
-                <li>♻️ Transaction in progress!</li>
-              )}
-              {!latestTransaction.isInProgress && (
-                <li>✅ Transaction completed!</li>
-              )}
-              {latestTransaction.toAddress && (
-                <li>
-                  To: {getUserTxnIsTo(latestTransaction.toAddress)?.first_name}{' '}
-                  {getUserTxnIsTo(latestTransaction.toAddress)?.last_name}
                 </li>
-              )}
-              {latestTransaction.txnExplorerUrl && (
-                <li>
-                  View Txn:
-                  <a
-                    className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                    href={latestTransaction.txnExplorerUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {latestTransaction.txnHash?.substring(0, 5)}...
-                    {latestTransaction.txnHash?.substring(60)}
-                  </a>
-                </li>
-              )}
-              {latestTransaction.estimatedTxnCost && (
-                <li>
-                  Estimated Txn Cost: {latestTransaction.estimatedTxnCost} AVAX
-                </li>
-              )}
-              {latestTransaction.actualTxnCost && (
-                <li>Actual Txn Cost: {latestTransaction.actualTxnCost} AVAX</li>
-              )}
+              ))}
             </ul>
-          </div>
-        )}
-
-        {userJwt && (
-          <div className="flex justify-center mt-10">
-            <div className="">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => {
-                  signOut();
-                  // TODO: This is hacky af
-                  setTimeout(() => {
-                    router.push('/');
-                  }, 500);
-                }}
-              >
-                Sign out
-              </button>
+          )}
+          {latestTransaction && (
+            <div className="mt-10">
+              <ul>
+                {latestTransaction.isInProgress && (
+                  <li>♻️ Transaction in progress!</li>
+                )}
+                {!latestTransaction.isInProgress && (
+                  <li>✅ Transaction completed!</li>
+                )}
+                {latestTransaction.toAddress && (
+                  <li>
+                    To:{' '}
+                    {getUserTxnIsTo(latestTransaction.toAddress)?.first_name}{' '}
+                    {getUserTxnIsTo(latestTransaction.toAddress)?.last_name}
+                  </li>
+                )}
+                {latestTransaction.txnExplorerUrl && (
+                  <li>
+                    View Txn:
+                    <a
+                      className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+                      href={latestTransaction.txnExplorerUrl}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {latestTransaction.txnHash?.substring(0, 5)}...
+                      {latestTransaction.txnHash?.substring(60)}
+                    </a>
+                  </li>
+                )}
+                {latestTransaction.estimatedTxnCost && (
+                  <li>
+                    Estimated Txn Cost: {latestTransaction.estimatedTxnCost}{' '}
+                    AVAX
+                  </li>
+                )}
+                {latestTransaction.actualTxnCost && (
+                  <li>
+                    Actual Txn Cost: {latestTransaction.actualTxnCost} AVAX
+                  </li>
+                )}
+              </ul>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/shared_components/navBar.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cx from 'classnames';
+
+interface Props {
+  signOut: () => void;
+  active: 'home' | 'send' | 'profile';
+}
+export default function NavBar({ signOut, active }: Props) {
+  const router = useRouter();
+  const buttonClass =
+    'bg-gray-200 disabled:bg-gray-400 hover:bg-blue-100 text-black py-1 px-2 rounded';
+  return (
+    <nav className="fixed bottom-0 bg-gray-100 w-full h-12 p-2 mt-0 z-10">
+      <div className=" flex justify-center content-center">
+        <ul className="flex flex-row text-center w-full md:w-1/4 gap-x-2">
+          <li className="basis-1/4">
+            <button
+              className={cx(buttonClass, { 'bg-blue-200': active === 'home' })}
+            >
+              <Link href="/home">Home</Link>
+            </button>
+          </li>
+
+          <li className="basis-1/4">
+            <button
+              className={cx(buttonClass, { 'bg-blue-200': active === 'send' })}
+            >
+              <Link href="/demo">Send</Link>
+            </button>
+          </li>
+
+          <li className="basis-1/4">
+            <button className={buttonClass} disabled>
+              Profile
+            </button>
+          </li>
+
+          <li className="basis-1/4">
+            <button
+              className={buttonClass}
+              onClick={() => {
+                signOut();
+                // TODO: This is hacky af
+                setTimeout(() => {
+                  router.push('/');
+                }, 500);
+              }}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
