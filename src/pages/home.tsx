e597721f29bd002c2f0652b33fa9faa0113897f9
Mike Alicea <mfalicea58@gmail.com>
2022-06-04 13:12:27 -0500

    HF: Fix link /demo to /send

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -126,12 +126,12 @@ const Home = ({ self }: Props) => {
                 <ul className="mt-2 flex flex-col items-start gap-y-3 overflow-auto">
                   <li>
                     <button className="bg-blue-500 disabled:bg-gray-400 hover:bg-blue-700 text-white py-1 px-2 rounded">
-                      <Link href="/demo">5 token tip: Kindness</Link>
+                      <Link href="/send">5 token tip: Kindness</Link>
                     </button>
                   </li>
                   <li>
                     <button className="bg-blue-500 disabled:bg-gray-400 hover:bg-blue-700 text-white py-1 px-2 rounded">
-                      <Link href="/demo">10 token tip: Support</Link>
+                      <Link href="/send">10 token tip: Support</Link>
                     </button>
                   </li>
                 </ul>
