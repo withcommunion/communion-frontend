bf8a6bed3d9b9dd0559546c1f91f52474652fc1f
Mike Alicea <mfalicea58@gmail.com>
2022-06-18 14:06:51 -0500

    Adds initial store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import '@aws-amplify/ui-react/styles.css';
 import type { AppProps } from 'next/app';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { Provider } from 'react-redux';
 
 import { UserContextProvider } from '@/context/userContext';
 import { AMPLIFY_CONFIG } from '../util/cognitoAuthUtil';
+import store from '@/reduxStore';
 
 // https://docs.amplify.aws/lib/client-configuration/configuring-amplify-categories/q/platform/js/#general-configuration
 Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
@@ -13,11 +15,13 @@ Amplify.configure({ ...AMPLIFY_CONFIG, ssr: true });
 function App({ Component, pageProps }: AppProps) {
   // TODO: This may cause continual re-renders.  Look here https://ui.docs.amplify.aws/react/components/authenticator#prevent-re-renders=
   return (
-    <UserContextProvider>
-      <Authenticator.Provider>
-        <Component {...pageProps} />
-      </Authenticator.Provider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <Authenticator.Provider>
+          <Component {...pageProps} />
+        </Authenticator.Provider>
+      </UserContextProvider>
+    </Provider>
   );
 }
 
