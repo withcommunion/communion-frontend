97a8e1db37c4435b0dd4c3602750f2980360a905
Mike Alicea <mfalicea58@gmail.com>
2022-06-26 16:35:55 -0500

    HF: Make component start with Link

--- src/pages_components/home/shortcutAction.tsx
+++ src/pages_components/home/shortcutAction.tsx
@@ -8,30 +8,28 @@ interface Props {
 
 const ShortcutAction = ({ actionAmount, actionName }: Props) => {
   return (
-    <>
+    <Link href="/community" passHref>
       <button className="bg-slate-200 p-2 rounded-lg hover:bg-slate-400">
-        <Link href="/community" passHref={true}>
-          <div className="relative">
-            <div className="flex items-center min-w-55vw md:min-w-15vw max-h-10vh">
-              <span className="text-black text-4xl p-1">{actionAmount}</span>
-              <div className="flex flex-col p-2">
-                <span className="text-xl text-black">Token tip</span>
-                <span className="text-xl text-black">{actionName}</span>
-              </div>
-            </div>
-            <div className="absolute -right-8 -top-1 overflow-visible z-100">
-              <Image
-                className=""
-                width="90"
-                height="85"
-                src={'/images/cupcake.png'}
-                alt="kindness cupcake"
-              />
+        <div className="relative">
+          <div className="flex items-center min-w-55vw md:min-w-15vw max-h-10vh">
+            <span className="text-black text-4xl p-1">{actionAmount}</span>
+            <div className="flex flex-col p-2">
+              <span className="text-xl text-black">Token tip</span>
+              <span className="text-xl text-black">{actionName}</span>
             </div>
           </div>
-        </Link>
+          <div className="absolute -right-8 -top-1 overflow-visible z-100">
+            <Image
+              className=""
+              width="90"
+              height="85"
+              src={'/images/cupcake.png'}
+              alt="kindness cupcake"
+            />
+          </div>
+        </div>
       </button>
-    </>
+    </Link>
   );
 };
 
