aac34faf1b7e517eb8c9edb240c04a9b19ad7f95
Mike Alicea <mfalicea58@gmail.com>
2022-07-22 12:07:32 -0500

    Adds fetchOrgRedeem actions

--- src/features/cart/cartSlice.tsx
+++ src/features/cart/cartSlice.tsx
@@ -1,22 +1,36 @@
+import axios from 'axios';
+import { Transaction } from 'ethers';
 import {
   createSlice,
   createSelector,
   PayloadAction,
   nanoid,
+  createAsyncThunk,
 } from '@reduxjs/toolkit';
 import type { RootState } from '@/reduxStore';
 
-import { OrgRedeemable } from '@/util/walletApiUtil';
+import { OrgRedeemable, DEV_API_URL } from '@/util/walletApiUtil';
+import { HTTPSProvider } from '@/util/avaxEthersUtil';
 
 interface OrgRedeemableInCart extends OrgRedeemable {
   id: string;
 }
 interface CartState {
   cart: OrgRedeemableInCart[];
+  latestRedeemTxn: {
+    txn: Transaction | null;
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null | undefined;
+  };
 }
 
 const initialState: CartState = {
   cart: [],
+  latestRedeemTxn: {
+    txn: null,
+    status: 'idle',
+    error: null,
+  },
 };
 
 const cartSlice = createSlice({
@@ -50,6 +64,25 @@ const cartSlice = createSlice({
       );
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchOrgRedeem.pending, (state) => {
+        state.latestRedeemTxn.status = 'loading';
+      })
+      .addCase(fetchOrgRedeem.fulfilled, (state, action) => {
+        state.latestRedeemTxn.status = 'succeeded';
+        // Add any fetched posts to the array
+        if (action.payload) {
+          state.latestRedeemTxn.error = null;
+          state.latestRedeemTxn.txn = action.payload;
+        }
+      })
+      .addCase(fetchOrgRedeem.rejected, (state, action) => {
+        state.latestRedeemTxn.txn = null;
+        state.latestRedeemTxn.status = 'failed';
+        state.latestRedeemTxn.error = action.error.message;
+      });
+  },
 });
 
 export default cartSlice.reducer;
@@ -70,4 +103,66 @@ export const selectTotalCost = createSelector([selectCart], (cart) =>
   cart.reduce((acc, redeemable) => {
     return acc + parseInt(redeemable.amount);
   }, 0)
+);
+
+export const fetchOrgRedeem = createAsyncThunk(
+  'cart/fetchOrgRedeem',
+  async ({
+    amount,
+    orgId,
+    jwtToken,
+  }: {
+    amount: number;
+    orgId: string;
+    jwtToken: string;
+  }) => {
+    const waitXSeconds = (seconds: number) =>
+      new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+    try {
+      const txnResp = await axios.post<{
+        transaction: Transaction;
+        txnHash: string;
+      }>(
+        `${DEV_API_URL}/org/${orgId}/redeem`,
+        {
+          amount,
+        },
+        {
+          headers: {
+            Authorization: jwtToken,
+          },
+        }
+      );
+
+      /**
+       * TODO: This is janky, how does uniswap or other handle this?
+       * We will want some retry logic to consistently
+       */
+      await waitXSeconds(3.5);
+      const ethersTxn = await HTTPSProvider.getTransaction(
+        txnResp.data.txnHash
+      );
+      if (ethersTxn) {
+        await ethersTxn.wait();
+      }
+      return txnResp.data.transaction;
+      // @ts-expect-error this is okay
+    } catch (error: axios.AxiosError) {
+      console.error(error);
+      if (axios.isAxiosError(error)) {
+        /**
+         * TODO: This is not ideal.  We can surface this error better on the BE
+         */
+        // @ts-expect-error this is okay for now.
+        // eslint-disable-next-line
+        const errorFromBlockchain = error.response?.data?.error?.error
+          ?.reason as string;
+        if (errorFromBlockchain) {
+          throw new Error(errorFromBlockchain);
+        } else {
+          throw error.response?.data;
+        }
+      }
+    }
+  }
 );
