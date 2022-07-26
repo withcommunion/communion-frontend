d19c00353b5245b88e4edeae09f267eb15622cb3
Mike Alicea <mfalicea58@gmail.com>
2022-07-26 15:59:45 -0500

    HF: Update client id

--- src/util/cognitoAuthUtil.ts
+++ src/util/cognitoAuthUtil.ts
@@ -7,7 +7,7 @@ export const USER_POOL_ID_DEV = 'us-east-1_EXRZZF0cp';
 export const USER_POOL_CLIENT_ID_DEV = '4eerlu1taf72c8r20pv2tmmvmt';
 
 export const USER_POOL_ID_PROD = 'us-east-1_SeeaUyuuH';
-export const USER_POOL_CLIENT_ID_PROD = '66eoq77778g7d8e36v6pobj0b6';
+export const USER_POOL_CLIENT_ID_PROD = '4sdv5ic4kdhhggf7chcveocimk';
 
 const isProd = Boolean(process.env.NEXT_PUBLIC_VERCEL_STAGE === 'prod');
 const isDev = Boolean(process.env.NEXT_PUBLIC_VERCEL_STAGE === 'dev');
