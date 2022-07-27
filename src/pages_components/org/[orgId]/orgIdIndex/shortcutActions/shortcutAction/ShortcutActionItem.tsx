fdf1b150f1f8ab627a4a5885e60c30a4eb6801a4
Mike Alicea <mfalicea58@gmail.com>
2022-07-27 15:20:15 -0500

    Wireup shortcut actions to interact with multisend

--- src/pages_components/org/[orgId]/orgIdIndex/shortcutActions/shortcutAction/ShortcutActionItem.tsx
+++ src/pages_components/org/[orgId]/orgIdIndex/shortcutActions/shortcutAction/ShortcutActionItem.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 import { OrgAction } from '@/util/walletApiUtil';
 
 interface Props {
+  orgId: string;
   action: OrgAction;
   imageUrl: string;
+  onClick: () => void;
 }
 
-const ShortcutAction = ({ action, imageUrl }: Props) => {
+const ShortcutAction = ({ action, imageUrl, orgId, onClick }: Props) => {
   const { name, amount } = action;
   return (
-    <li>
-      <Link href="@/shared_components/tokenTip/TokenTip#">
+    <li onClick={onClick}>
+      <Link href={`/org/${orgId}/send`}>
         <a className="text-primaryGray my-2.5 bg-white justify-between rounded-xl flex items-center px-4">
           <div className="flex items-center">
             <span className="font-bold text-45px mr-3 py-4">{amount}</span>
