dffe53c68eb719b4c23b1a0104c97d5be824c7db
Mike Alicea <mfalicea58@gmail.com>
2022-07-27 11:53:05 -0500

    Puts in shortcut actions to real data

--- src/pages_components/org/[orgId]/orgIdIndex/shortcutActions/ShortcutActions.tsx
+++ src/pages_components/org/[orgId]/orgIdIndex/shortcutActions/ShortcutActions.tsx
@@ -1,20 +1,19 @@
 import ShortcutAction from '@/pages_components/org/[orgId]/orgIdIndex/shortcutActions/shortcutAction/ShortcutAction';
-
-export interface IShortcutActions {
-  name: string;
-  value: string | number;
-  image: string;
-}
+import { OrgAction } from '@/util/walletApiUtil';
 
 interface Props {
-  shortcutActions: IShortcutActions[];
+  shortcutActions: OrgAction[];
 }
-
+const images = [
+  '/images/home/tokenTips/Compliance.png',
+  '/images/home/tokenTips/Kindness.png',
+  '/images/home/tokenTips/Politeness.png',
+];
 const ShortcutActions = ({ shortcutActions }: Props) => {
   return (
     <>
       {shortcutActions.map((action, num: number) => (
-        <ShortcutAction key={num} action={action} />
+        <ShortcutAction key={num} action={action} imageUrl={images[num]} />
       ))}
     </>
   );
