ac962b693ab0f0b64615e5e61e50dd14814a9be4
Mike Alicea <mfalicea58@gmail.com>
2022-07-27 10:35:43 -0500

    Uses updated OrgTransactionHistoryList name

--- src/pages_components/org/[orgId]/orgIdIndexComponents.tsx
+++ src/pages_components/org/[orgId]/orgIdIndexComponents.tsx
@@ -1,2 +1,2 @@
 export { default as ShortcutActions } from './orgIdIndex/shortcutActions/ShortcutActions';
-export { default as OrgTransactionHistoryList } from './orgIdIndex/history/HistoryOrg';
+export { default as OrgTransactionHistoryList } from './orgIdIndex/history/OrgTransactionHistoryList';
