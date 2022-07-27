45153b1e1890994cc540ca016a2f86e10b3e7e81
Mike Alicea <mfalicea58@gmail.com>
2022-07-26 21:06:27 -0500

    Base input of multisend slice (copy from transactionsSlice)

---
+++ src/features/multisend/multisendSlice.ts
@@ -0,0 +1,152 @@
+import axios from 'axios';
+import {
+  createSlice,
+  createAsyncThunk,
+  createSelector,
+} from '@reduxjs/toolkit';
+import type { RootState } from '@/reduxStore';
+import { Transaction } from 'ethers';
+import { HTTPSProvider } from '@/util/avaxEthersUtil';
+
+import { API_URL, HistoricalTxn } from '@/util/walletApiUtil';
+
+interface MultisendState {
+  latestTxn: {
+    txn: Transaction | null;
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null | undefined;
+  };
+}
+
+// Define the initial state using that type
+const initialState: MultisendState = {
+  latestTxn: {
+    txn: null,
+    status: 'idle',
+    error: 'null',
+  },
+};
+
+export const multisendSlice = createSlice({
+  name: 'transactions',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMultisendFunds.pending, (state) => {
+        state.latestTxn.status = 'loading';
+      })
+      .addCase(fetchMultisendFunds.fulfilled, (state, action) => {
+        state.latestTxn.status = 'succeeded';
+        // Add any fetched posts to the array
+        if (action.payload) {
+          state.latestTxn.error = null;
+          state.latestTxn.txn = action.payload;
+        }
+      })
+      .addCase(fetchMultisendFunds.rejected, (state, action) => {
+        state.latestTxn.txn = null;
+        state.latestTxn.status = 'failed';
+        state.latestTxn.error = action.error.message;
+      });
+  },
+});
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectRootLatestTxn = (state: RootState) =>
+  state.multisend.latestTxn;
+export const selectLatestTxn = createSelector(
+  [selectRootLatestTxn],
+  (multisend) => multisend.txn
+);
+export const selectLatestTxnStatus = createSelector(
+  [selectRootLatestTxn],
+  (multisend) => multisend.status
+);
+export const selectLatestTxnErrorMessage = createSelector(
+  [selectRootLatestTxn],
+  (multisend) => multisend.error
+);
+
+export const fetchMultisendFunds = createAsyncThunk(
+  'transactions/fetchMultisendFunds',
+  async ({
+    toUserIdsAmountMap,
+    orgId,
+    jwtToken,
+  }: {
+    toUserIdsAmountMap: string[];
+    amount: number;
+    orgId: string;
+    jwtToken: string;
+  }) => {
+    const waitXSeconds = (seconds: number) =>
+      new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+    try {
+      const txnResp = await axios.post<{
+        transaction: Transaction;
+        txnHash: string;
+      }>(
+        `${API_URL}/user/self/transfer`,
+        {
+          orgId,
+          toUserIdsAmountMap,
+        },
+        {
+          headers: {
+            Authorization: jwtToken,
+          },
+        }
+      );
+
+      /**
+       * TODO: This is janky, how does uniswap or other handle this?
+       * We will want some retry logic to consistently
+       */
+      await waitXSeconds(3.5);
+      const ethersTxn = await HTTPSProvider.getTransaction(
+        txnResp.data.txnHash
+      );
+      if (ethersTxn) {
+        await ethersTxn.wait();
+      }
+      return txnResp.data.transaction;
+      // @ts-expect-error this is okay
+    } catch (error: axios.AxiosError) {
+      console.error(error);
+      if (axios.isAxiosError(error)) {
+        /**
+         * TODO: This is not ideal.  We can surface this error better on the BE
+         */
+        // @ts-expect-error this is okay for now.
+        // eslint-disable-next-line
+        const errorFromBlockchain = error.response?.data?.error?.error
+          ?.reason as string;
+        if (errorFromBlockchain) {
+          throw new Error(errorFromBlockchain);
+        } else {
+          throw error.response?.data;
+        }
+      }
+    }
+  }
+);
+
+export const fetchSelfHistoricalTxns = createAsyncThunk(
+  'transactions/fetchSelfHistoricalTxns',
+  async ({ orgId, jwtToken }: { orgId: string; jwtToken: string }) => {
+    const rawWallet = await axios.get<{ txs: HistoricalTxn[] }>(
+      `${API_URL}/org/${orgId}/txs/self`,
+      {
+        headers: {
+          Authorization: jwtToken,
+        },
+      }
+    );
+    const selfTxs = rawWallet.data;
+    return selfTxs.txs;
+  }
+);
+
+export default multisendSlice.reducer;
