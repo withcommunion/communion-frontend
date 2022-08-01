6d6b323a8220ffb202b57db0c4002cffe01347b3
Mike Alicea <mfalicea58@gmail.com>
2022-08-01 09:35:05 -0500

    Moves welcome to index page compoennts

--- src/pages_components/index/welcome/welcome.tsx
+++ src/pages_components/index/welcome/welcome.tsx
@@ -13,15 +13,10 @@ const Welcome = () => {
         <span className="font-paytoneOne text-fifthOrange text-6xl z-10">
           Welcome
         </span>
-        <span className="text-primaryGray font-semibold text-xl pt-3 z-10">
-          Create an account
+        <span className="text-primaryGray font-semibold text-xl pt-3 z-10 mb-8">
+          Please sign in or create an account
         </span>
-        <span className="font-light text-13px text-fifthGray z-10">
-          Sign up with your name, email and (optional)
-        </span>
-        <span className="font-light text-13px text-fifthGray z-10">
-          username/Alias
-        </span>
+
         <div className="absolute -right-16 top-9">
           <Image
             src="/images/signUp/firstBackDecoration.svg"
