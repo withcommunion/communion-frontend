27e69d85608e88910bed1f0952359bb57f77f3ed
Mike Alicea <mfalicea58@gmail.com>
2022-08-05 13:55:24 -0500

    Adds checkbox styles to redeemableCard

--- src/pages_components/org/[orgId]/redeem/redeemableCard/redeemableCard.tsx
+++ src/pages_components/org/[orgId]/redeem/redeemableCard/redeemableCard.tsx
@@ -42,7 +42,23 @@ const RedeemableCard = ({ redeemable, toggleChecked, isChecked }: Props) => {
         </span>
         <input
           type="checkbox"
-          className={isChecked ? 'mx-4' : 'mx-5'}
+          className={`${isChecked ? 'mx-4' : 'mx-5'} appearance-none
+          relative
+          bg-secondaryLightGray
+          border-1px
+          border-secondaryPurple
+          h-4 w-4
+          cursor-pointer
+          transition-all
+          rounded inline-flex text-center justify-center items-center
+          checked:bg-secondaryLightGray
+          checked:after:border-b-[3px]
+          checked:after:border-primaryPurple
+          checked:after:border-r-[3px]
+          checked:after:rotate-45
+          checked:after:rounded-md
+          after:h-2.5 after:w-1.5
+          after:absolute`}
           checked={isChecked}
           onChange={() => undefined}
         ></input>
