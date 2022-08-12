4a9fb1936d21850bec76b795a0588ca95c5a2431
Mike Alicea <mfalicea58@gmail.com>
2022-08-12 17:11:21 -0500

    Cleans up routing and when to display what

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { getUserJwtTokenOnServer } from '@/util/cognitoAuthUtil';
 
@@ -9,8 +10,10 @@ import { selectSelf, selectSelfStatus, fetchSelf } from '@/features/selfSlice';
 import {
   fetchJoinOrgById,
   selectLatestJoinedOrgStatus,
+  selectLatestJoinedOrgErrorMsg,
 } from '@/features/joinOrg/joinOrgSlice';
 import { IndexHeader } from '@/pages_components/indexPageComponents';
+import Footer from '@/shared_components/footer/footer';
 
 const HomePage = ({ userJwt }: { userJwt: string }) => {
   const router = useRouter();
@@ -26,17 +29,24 @@ const HomePage = ({ userJwt }: { userJwt: string }) => {
   const latestJoinedOrgStatus = useAppSelector((state) =>
     selectLatestJoinedOrgStatus(state)
   );
+  const latestJoinedOrgErrorMsg = useAppSelector((state) =>
+    selectLatestJoinedOrgErrorMsg(state)
+  );
 
   useEffect(() => {
     const shouldFetchSelf = selfStatus === 'idle';
+    if (shouldFetchSelf && userJwt) {
+      dispatch(fetchSelf(userJwt));
+    }
+  }, [userJwt, selfStatus, dispatch]);
+
+  useEffect(() => {
     const shouldRouteUserToOnlyOrg =
       selfStatus === 'succeeded' &&
       self?.organizations.length === 1 &&
       !queryOrgId;
 
-    if (shouldFetchSelf) {
-      dispatch(fetchSelf(userJwt));
-    } else if (shouldRouteUserToOnlyOrg) {
+    if (shouldRouteUserToOnlyOrg) {
       router.push(`/org/${self.organizations[0].orgId}`);
     }
   }, [dispatch, selfStatus, self, queryOrgId, userJwt, router]);
@@ -83,23 +93,80 @@ const HomePage = ({ userJwt }: { userJwt: string }) => {
   ]);
 
   return (
-    <div className="py-4 flex flex-col items-center ">
-      {self && (
-        <div>
+    <div className="bg-secondaryLightGray pb-2 min-h-100vh ">
+      <div className="container flex w-full px-6 my-0 mx-auto my-auto md:max-w-50vw">
+        <div className="mt-5">
           <IndexHeader userName={self?.first_name} />
-          <h2 className="text-xl">Available groups</h2>
-          <ul>
-            {self?.organizations.map((org) => (
-              <li key={org.orgId}>{org.orgId}</li>
-            ))}
-          </ul>
+          {(selfStatus === 'idle' || selfStatus === 'loading') && (
+            <div className="text-center">
+              <span>Setting you up!...</span>
+              <Footer />
+            </div>
+          )}
+          {selfStatus === 'succeeded' && self && !queryOrgId && (
+            <div className="flex flex-col">
+              {self.organizations.length ? (
+                <div className="text-center ">
+                  <h2 className="text-xl mt-5">Available groups</h2>
+                  <ul>
+                    {self.organizations.map((org) => (
+                      <li key={org.orgId} className="my-2">
+                        <Link href={`/org/${org.orgId}`}>
+                          <button className="bg-secondaryOrange w-275px h-46px text-white shadow-primaryButtonShadow">
+                            {org.orgId}
+                          </button>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                <div className="mt-5">
+                  <h2 className="text-2xl text-center">Hmmmm...</h2>
+                  <h3 className="text-xl text-start">
+                    It looks like you are not a member of any groups.
+                  </h3>
+                  <p>
+                    If you have an invite link, please paste that into your
+                    browser!
+                  </p>
+                  <Footer />
+                </div>
+              )}
+            </div>
+          )}
+
+          {latestJoinedOrgStatus === 'loading' && (
+            <div className="mt-5">
+              <h2 className="text-2xl text-center">
+                Joining the organization!
+              </h2>
+              <Footer />
+            </div>
+          )}
+          {latestJoinedOrgStatus === 'failed' && (
+            <div className="mt-5">
+              <h2 className="text-2xl">Hmmmm...Something went wrong</h2>
+              <p className="text-xl text-start my-2">
+                It looks like the organization you are trying to join does not
+                exist.
+              </p>
+              <p className="text-xl text-start my-2">
+                Or the joinCode you were given is wrong.
+              </p>
+              <p>
+                If you have an invite link, please paste that into your browser!
+              </p>
+              <p>
+                If you already did that, please ask your organization admin to
+                resend the url.
+              </p>
+              <p className="mt-5">{latestJoinedOrgErrorMsg}</p>
+              <Footer />
+            </div>
+          )}
         </div>
-      )}
-      {selfStatus === 'idle' && (
-        <div>
-          <span>Fetching you from the database!...</span>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
