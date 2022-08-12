8d95c2a5fc6bcfc1056ec843392f5fcbafc34132
Mike Alicea <mfalicea58@gmail.com>
2022-08-12 17:11:49 -0500

    Simple centering of text

--- src/pages_components/index/indexHeader.tsx
+++ src/pages_components/index/indexHeader.tsx
@@ -3,12 +3,12 @@ interface Props {
 }
 const IndexHeader = ({ userName }: Props) => {
   return (
-    <>
+    <div className="text-center">
       <h1 className="text-2xl">
         {userName ? `👋 Hey ${userName}` : '👋 Hey friend'}
       </h1>
       <h1 className="text-3xl">Welcome to Communion!</h1>
-    </>
+    </div>
   );
 };
 
