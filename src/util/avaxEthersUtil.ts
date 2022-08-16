7d766eb8cba9d2d69a201957659de0f1be29f3bd
Mike Alicea <mfalicea58@gmail.com>
2022-08-16 11:18:34 -0500

    HF: Remove Avalanche library

--- src/util/avaxEthersUtil.ts
+++ src/util/avaxEthersUtil.ts
@@ -1,4 +1,3 @@
-import Avalanche from 'avalanche';
 import { ethers } from 'ethers';
 
 import { isProd } from '@/util/envUtil';
@@ -7,15 +6,6 @@ export const prodAvaxUrl = 'api.avalanche.network';
 export const fujiTestAvaxUrl = 'api.avax-test.network';
 export const prodAvaxRpcUrl = 'https://api.avax.network/ext/bc/C/rpc';
 export const fujiTestAvaxRpcUrl = 'https://api.avax-test.network/ext/bc/C/rpc';
-
-const chainId = 43113;
-const avalanche = new Avalanche(
-  isProd ? prodAvaxUrl : fujiTestAvaxUrl,
-  undefined,
-  'https',
-  chainId
-);
-const cchain = avalanche.CChain();
 
 export const HTTPSProvider = new ethers.providers.JsonRpcProvider(
   isProd ? prodAvaxRpcUrl : fujiTestAvaxRpcUrl
@@ -26,77 +16,6 @@ export function getEthersWallet(
 ): ethers.Wallet {
   return new ethers.Wallet(privateKeyWithLeadingHex, HTTPSProvider);
 }
-
-/**
- * Base code taken from https://docs.avax.network/quickstart/sending-transactions-with-dynamic-fees-using-javascript/#function-for-estimating-max-fee-and-max-priority-fee
- * Info on maxFeePerGas and maxPriorityFeePerGas: https://docs.alchemy.com/alchemy/guides/eip-1559/maxpriorityfeepergas-vs-maxfeepergas
- *
- * Terms:
- * baseFee: set by network to perform transaction.  Gets burned and does not go to miner.
- * maxPriorityFeePerGas: This is the "tip" that goes to the miner.
- *  The higher the tip, the more likely the transaction will be included in a block.
- * maxFeePerGas: This is the total amount of gas used in the transaction.  Tip + Bare minimum set by network
- * Gwei and nAvax are the same thing
- */
-export const getAvaxChainBaseFees = async () => {
-  const baseFee = parseInt(await cchain.getBaseFee(), 16) / 1e9;
-  const maxPriorityFeePerGas =
-    parseInt(await cchain.getMaxPriorityFeePerGas(), 16) / 1e9;
-
-  const maxFeePerGas = baseFee + maxPriorityFeePerGas;
-
-  if (maxFeePerGas < maxPriorityFeePerGas) {
-    throw new Error(
-      'Max fee per gas cannot be less than max priority fee per gas'
-    );
-  }
-
-  const maxFees = {
-    maxFeePerGasGwei: maxFeePerGas.toString(),
-    maxPriorityFeePerGasGwei: maxPriorityFeePerGas.toString(),
-  };
-
-  return maxFees;
-};
-
-export const createBaseTxn = async (
-  fromAddress: string,
-  amount: string,
-  toAddress: string
-): Promise<ethers.providers.TransactionRequest> => {
-  const MAX_GAS_WILLING_TO_SPEND_GWEI = '45';
-
-  const nonce = await HTTPSProvider.getTransactionCount(fromAddress);
-  const { maxFeePerGasGwei, maxPriorityFeePerGasGwei } =
-    await getAvaxChainBaseFees();
-
-  if (maxFeePerGasGwei > MAX_GAS_WILLING_TO_SPEND_GWEI) {
-    console.log(`Spending more than MAX_GWEI_GAS_WILLING_TO_SPEND`, {
-      MAX_GAS_WILLING_TO_SPEND_GWEI,
-      maxFeePerGasGwei,
-    });
-    throw new Error(`Spending more than MAX_GWEI_GAS_WILLING_TO_SPEND`);
-  }
-
-  const maxFeePerGasInAvax = ethers.utils.parseUnits(maxFeePerGasGwei, 'gwei');
-  const maxPriorityFeePerGasInAvax = ethers.utils.parseUnits(
-    maxPriorityFeePerGasGwei,
-    'gwei'
-  );
-
-  const baseTx: ethers.providers.TransactionRequest = {
-    // Type 2 transaction is for EIP1559 (https://eips.ethereum.org/EIPS/eip-1559)
-    type: 2,
-    nonce,
-    to: toAddress,
-    maxPriorityFeePerGas: maxPriorityFeePerGasInAvax,
-    maxFeePerGas: maxFeePerGasInAvax,
-    value: ethers.utils.parseEther(amount),
-    chainId,
-  };
-
-  return baseTx;
-};
 
 export const getEstimatedTxnCosts = async (
   baseTxn: ethers.providers.TransactionRequest
