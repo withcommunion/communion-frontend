c37ab8801b4afeeea05751177c5b704b8d00ebbe
Mike Alicea <mfalicea58@gmail.com>
2022-10-04 14:19:37 -0400

    HF: Adds Iframe

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -2,7 +2,16 @@ const LandingPage = () => {
   return (
     <div className="min-h-100vh bg-secondaryLightGray pb-2 ">
       <div className="container my-0 mx-auto flex w-full justify-center px-6 md:max-w-50vw"></div>
-      <p>Placeholder</p>
+      <div className="h-0 w-100vw">
+        <iframe
+          allowFullScreen
+          className="h-75vh w-75vw"
+          src="https://www.beautiful.ai/embed/-NDEC9_cwOc82ysWvrNe?utm_source=beautiful_player&utm_medium=embed&utm_campaign=-NC0yRqzCjtieL2CA_e1"
+        ></iframe>
+        <a href="https://www.beautiful.ai/embed/-NDEC9_cwOc82ysWvrNe?utm_source=beautiful_player&utm_medium=embed&utm_campaign=-NC0yRqzCjtieL2CA_e1">
+          Communion Overview
+        </a>
+      </div>
     </div>
   );
 };
