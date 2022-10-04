2e0c0e9f032e1644d85857075dba17553371cb47
Mike Alicea <mfalicea58@gmail.com>
2022-10-04 14:14:29 -0400

    HF: Placeholder for landing page

---
+++ src/pages/landing.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div className="min-h-100vh bg-secondaryLightGray pb-2 ">
+      <div className="container my-0 mx-auto flex w-full justify-center px-6 md:max-w-50vw"></div>
+      <p>Placeholder</p>
+    </div>
+  );
+};
+
+export default LandingPage;
